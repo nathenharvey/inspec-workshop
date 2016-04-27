b1e3f77e77715ceeaa2ade56a447e98409d0d729
Nathen Harvey <nharvey@chef.io>
2016-04-27 13:26:10 -0600

    Verify CentOS GPG Key is Installed

--- filesystem_configuration/controls/example.rb
+++ filesystem_configuration/controls/example.rb
@@ -2,19 +2,28 @@
 # copyright: 2015, The Authors
 # license: All rights reserved
 
-title 'sample section'
+title 'File System Configuration'
 
-# you can also use plain tests
-describe file('/tmp') do
-  it { should be_directory }
-end
+c = command('/usr/bin/gpg --quiet --with-fingerprint /etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6')
 
 # you add controls here
-control 'tmp-1.0' do                        # A unique ID for this control
-  impact 0.7                                # The criticality, if this control fails.
-  title 'Create /tmp directory'             # A human-readable title
-  desc 'An optional description...'
-  describe file('/tmp') do                  # The actual test
-    it { should be_directory }
+control '1.2.1 Verify CentOS GPG Key is Installed' do                        # A unique ID for this control
+  impact 1.0                                # The criticality, if this control fails.
+  title 'Verify CentOS GPG Key is Installed'             # A human-readable title
+  desc <<-EOF
+    CentOS cryptographically signs updates with a GPG key to verify that they are valid.
+
+    It is important to ensure that updates are obtained from a valid source to protect against spoofing that could lead to the inadvertent installation of malware on the system.
+  EOF
+
+  describe c do
+    its('stdout') { should match /C1DA C52D 1664 E8A4 386D  BA43 0946 FCA2 C105 B9DE/ }
+  end
+
+  describe command('echo hello') do
+    its('stdout') { should match /hello/ }
   end
 end
+
+
+# node26-denver-popup2.training.chef-demo.com
