e8f04690a45c8c16fc710262765f202b584547ad
Nathen Harvey <nharvey@chef.io>
2016-04-27 13:53:18 -0600

    clean up the comments

--- filesystem_configuration/controls/example.rb
+++ filesystem_configuration/controls/example.rb
@@ -4,26 +4,17 @@
 
 title 'File System Configuration'
 
-c = command('/usr/bin/gpg --quiet --with-fingerprint /etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6')
-
 # you add controls here
-control '1.2.1 Verify CentOS GPG Key is Installed' do                        # A unique ID for this control
-  impact 1.0                                # The criticality, if this control fails.
-  title 'Verify CentOS GPG Key is Installed'             # A human-readable title
+control '1.2.1 Verify CentOS GPG Key is Installed' do
+  impact 1.0
+  title 'Verify CentOS GPG Key is Installed'
   desc <<-EOF
     CentOS cryptographically signs updates with a GPG key to verify that they are valid.
 
     It is important to ensure that updates are obtained from a valid source to protect against spoofing that could lead to the inadvertent installation of malware on the system.
   EOF
 
-  describe c do
+  describe command('/usr/bin/gpg --quiet --with-fingerprint /etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6') do
     its('stdout') { should match /C1DA C52D 1664 E8A4 386D  BA43 0946 FCA2 C105 B9DE/ }
   end
-
-  describe command('echo hello') do
-    its('stdout') { should match /hello/ }
-  end
 end
-
-
-# node26-denver-popup2.training.chef-demo.com
