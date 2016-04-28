5ee7b5aed1c9b01a2e7a77883b929f7f4abf47de
Nathen Harvey <nharvey@chef.io>
2016-04-27 23:02:33 -0600

    fix rubocop offenses
    Signed-off-by: Nathen Harvey <nharvey@chef.io>

--- os_services/controls/example.rb
+++ os_services/controls/example.rb
@@ -4,14 +4,17 @@
 
 title 'OS Services'
 
-
 control '2.1.1 Remove telnet-server' do
   impact 1.0
   title 'Remove telnet-server'
   desc <<-EOF
-    The telnet-server package contains the telnetd daemon, which accepts connections from users from other systems via the telnet protocol.
+    The telnet-server package contains the telnetd daemon, which accepts
+    connections from users from other systems via the telnet protocol.
 
-    The telnet protocol is insecure and unencrypted. The use of an unencrypted transmission medium could allow a user with access to sniff network traffic the ability to steal credentials. The ssh package provides an encrypted session and stronger security and is included in most Linux distributions.
+    The telnet protocol is insecure and unencrypted. The use of an unencrypted
+    transmission medium could allow a user with access to sniff network traffic
+    the ability to steal credentials. The ssh package provides an encrypted
+    session and stronger security and is included in most Linux distributions.
   EOF
   describe package('telnet-server') do
     it { should_not be_installed }
